eb63179408c86773acf8807367931fec10710e4c
MR-Titus02 <titusroxsan@gmail.com>
2025-04-24 12:53:12 +0530

    react route

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+function Contact() {
+  return (
+    <>
+    
+    
+    
+    
+    
+    </>
+  );
+}
+export default Contact;
